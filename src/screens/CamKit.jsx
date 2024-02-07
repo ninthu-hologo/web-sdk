a142a38bb157c3b4544caf675735a2cdc818b59a
ninthu-hologo <ninthu1999@gmail.com>
2024-02-07 10:22:47 +0530

    update groupid

--- src/screens/CamKit.jsx
+++ src/screens/CamKit.jsx
@@ -16,7 +16,7 @@ const CamKit = () => {
 
   const apiToken =
     "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjkxOTAzNjY2LCJzdWIiOiI3NDAyOTgxZS03N2M0LTQ0M2QtYjk2OS0yY2E3YjUzOWZmMjd-UFJPRFVDVElPTn42M2YwYTQ2Yy1jMmZmLTQxN2UtYTBkMC05YjM2MmM5NGUyZWUifQ.lit2odeqkuEKa5rR2o5e65nSuhk9gFgho95RFCPqAys";
-  const lensGroupId = "c8519e0c-54f4-4c78-9dc4-dae03a17d21e";
+  const lensGroupId = "bd8a9f04-477b-4336-994b-b4e1fcbf3200";
   const { currentLensIndex, setCurrentLensIndex } = useContext(AuthContext);
   const { lensesLength, setLensesLength } = useContext(AuthContext);
   const { lensesFetch, setLensesFetch } = useContext(AuthContext);
